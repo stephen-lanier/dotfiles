0a12f39b389aab4c897d699ca70502662a767f92
stephen-lanier <102939601+stephen-lanier@users.noreply.github.com>
2024-07-10 18:39:38 -0700

    various updates

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -29,7 +29,7 @@ return {
             lspconfig.jdtls.setup({})
             lspconfig.clangd.setup({})
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
         end,
     },
