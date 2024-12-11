ac85afc50e4e8246df0fc541e9ea4619f453aeb4
stephen-lanier <102939601+stephen-lanier@users.noreply.github.com>
2024-12-11 11:33:17 -0800

    add lsp rename shortcut

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -32,6 +32,8 @@ return {
             lspconfig.bashls.setup({})
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
         end,
     },
